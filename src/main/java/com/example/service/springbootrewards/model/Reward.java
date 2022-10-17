cea7975ac967b2b2d45a9dcca5e8cbfce2d154bb
PondaluS <surya.pondaluru@hp.com>
2022-10-17 13:42:40 -0500

    Rewards program.

---
+++ src/main/java/com/example/service/springbootrewards/model/Reward.java
@@ -0,0 +1,14 @@
+package com.example.service.springbootrewards.model;
+
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public abstract class Reward {
+
+    @JsonInclude
+    @Transient
+    protected Long points;
+
+    public abstract Long getPoints();
+}
---
+++ src/main/java/com/example/service/springbootrewards/rewards/RewardsController.java
@@ -0,0 +1,33 @@
+package com.example.service.springbootrewards.rewards;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.service.springbootrewards.model.Customer;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class RewardsController {
+
+    @Autowired
+    private RewardsService rewardsService;
+
+    @GetMapping("/customers")
+    public List<Customer> findCustomerAll() {
+        return rewardsService.getCustomerAll();
+    }
+
+    @GetMapping("/customers/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Integer id) {
+        Customer customer = rewardsService.getCustomerById(id);
+        if (customer == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+}
